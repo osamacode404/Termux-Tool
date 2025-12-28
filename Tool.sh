aa2e9019f372b35ebc1588d06953ced795e739d0
osamacode404 <ggosama137@gmail.com>
2025-12-28 12:43:12 +0200

    Add files via upload

--- Tool.sh
+++ Tool.sh
@@ -7,88 +7,131 @@ B='\e[1;34m'   # الأزرق (Blue)
 P='\e[1;35m'   # الأرجواني (Purple)
 C='\e[1;36m'   # السماوي (Cyan)
 W='\e[1;37m'   # الأبيض (White)
+TEXT='\e[1;37m' #text
 NC='\e[0m'
 
 ###################################################
 test1 ( ) {
-while true; do
-echo -e "${G}#####################################${NC}"
-echo -e "${G}#########${NC}${R} Welcome to my tool ${NC}${G}########${NC} "
-echo -e "${G}######${NC}${R} i'm engineer osama elsayed ${NC}${G}####${NC}"
-echo -e "${G}#####################################${NC}"
+clear
+echo -e "${G}##################################${NC}"
+echo -e "${G}######${NC}${R} Welcome to my tool ${NC}${G}########${NC} "
+echo -e "${G}###${NC}${R} i'm engineer osama elsayed ${NC}${G}###${NC}"
+echo -e "${G}##################################${NC}"
 echo ""
+sleep 1
+echo -e "${G}note${NC}:${R}More options will be added to the tool soon${NC}"
+sleep 1
+echo ""
+}
+test1
+test2 ( ) {
+while true; do
+echo -e "${C}  ___  ${G} __   __"
+echo -e "${C} / __| ${G} \ \ / /"
+echo -e "${C}| (__  ${G}  \ V / "
+echo -e "${C} \___| ${G}   \_/  "
+
 echo ""
 
-echo -e "[ 1 ] ==>>${C}Download everything you need for Termux..!${NC}"
-echo -e "[ 2 ] ==>>${C}To update and upgrade Termux..!${NC}"
-echo -e "[ 3 ] ==>>${C}To display information about the owner of the tool..!${NC}"
-echo -e "[ 4 ] ==>>${C}To exit the tool..!${NC}"
+echo -e "${TEXT}logo: ${P}codevirus${NC}"
 echo ""
-echo -e -n "${R}Enter the number !${R}: "
+sleep 1
+echo -e "[ 1 ] » ${C}Download everything you need for Termux..!${NC}"
+sleep 1
+echo -e "[ 2 ] » ${C}To update and upgrade Termux..!${NC}"
+sleep 1
+echo -e "[ 3 ] » ${C}To display information about the owner of the tool${NC}"
+sleep 1
+echo -e "[ 4 ] » ${C}Play with Bash the Christmas game.!${NC}"
+sleep 1
+echo -e "[ 0 ] » ${C}To exit the tool..!${NC}"
+sleep 1
+echo ""
+sleep 2
+
+echo -e -n "${R}Enter the number!${R}: "
 read num
 if [ "$num" == "1" ]; then
 pkg install python -y
 pkg install wget curl -y
 pkg install nano -y
 pkg install git -y
-pkg install python-pip -y
+pkg install pip -y
 pkg install php -y
 sleep 1
 echo ""
-echo -e "${G}Downloaded ✅${NC}"
+echo -e "${G}Downloaded${NC}✅"
 echo ""
 echo -e "${G}Home tools will be updated....${NC}"
 sleep 1
 clear
 pkg update -y && pkg upgrade -y
-sleep 2
 echo ""
+sleep 1
+echo -e "${G}Done${NC}✅"
 echo ""
-echo -e "${G}Done..✅${NC}"
-
 sleep 2
 clear
 
 elif [ "$num" == "2" ]; then
-sleep 1
 echo "In progress..."
 sleep 1
 clear
 pkg update -y && pkg upgrade -y
 echo ""
-echo -e "${G}Done..✅${NC}"
-sleep 4
+sleep 1
+echo -e "${G}Done${NC}✅"
+echo ""
+sleep 2
 
 clear
+
 
 elif [ "$num" == "3" ]; then
+echo ""
 sleep 1
-echo ""
 echo -e "${G}Tool developer: Engineer Osama${NC}"
+sleep 3
 echo ""
-sleep 2
-echo ""
-echo -e "${G}Done..✅${NC}"
-sleep 4
 clear
-
-
 elif [ "$num" == "4" ]; then
 sleep 1
-echo -e "${R}The tool has been exited.Bey..${NC}"
+echo -e "${G}Welcome${NC}"
+sleep 1
+echo -e "${G}Let's play${NC}"
+sleep 1
+echo -e "${G}I will tell you your birth year at the end of the game${NC}"
+sleep 1
+echo -e "${G}85. Subtract your age from that and tell me how much is ${NC}"
+
+sleep 1
+echo -e -n "${B}Fadel Kam?${NC}: "
+read num2
+
+natg=$(($num2 + 1940))
+sleep 1
+echo -e "${G}Your birth year:${NC}${R}$natg${NC}"
+sleep 3
+clear
+
+elif [ "$num" == "0" ]; then
+echo ""
 sleep 2
+echo -e "${G}Done, Bey 👋${NC}✅"
+echo ""
+sleep 1
+
 exit
 
 
 
 else
-sleep 2
+
 echo -e "${R}incorrect، try again..${NC}"
-sleep 2
-
-
+echo ""
 fi
 done
 
 }
-test1
+
+test2
